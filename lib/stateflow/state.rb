92e16ab6e0345848ad5c3ae0240ff11fd31465c9
Henk <henk.van.der.veen@gmail.com>
2012-02-19 23:33:14 +0000

    instead of overriding state methods(enter,exit) when redefining, add them as extra callbacks

--- lib/stateflow/state.rb
+++ lib/stateflow/state.rb
@@ -10,21 +10,21 @@ module Stateflow
     end
     
     def enter(method = nil, &block)
-      @options[:enter] = method.nil? ? block : method
+      (@options[:enter] ||= []) << (method.nil? ? block : method)
     end
     
     def exit(method = nil, &block)
-      @options[:exit] = method.nil? ? block : method
+      (@options[:exit] ||= []) << (method.nil? ? block : method)
     end
     
     def execute_action(action, base)
-      action = @options[action.to_sym]
-      
-      case action
-      when Symbol, String
-        base.send(action)
-      when Proc
-        action.call(base)
+      (@options[action.to_sym] || []).each do |cb|
+        case cb
+        when Symbol, String
+          base.send(cb)
+        when Proc
+          cb.call(base)
+        end
       end
     end
   end
